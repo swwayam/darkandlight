f8264fc323eb528274bae5ff202f2e1fdca0d0f6
swwayamm <codingswayam24@gmail.com>
2022-11-21 18:58:09 +0530

    themeSwticherContextLearn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from "react";
+import "./App.css";
+import ThemeContext from "./Context/ThemeContext";
+import Toggler from "./Components/Toggler";
+import HeroSection from "./Components/HeroSection"; 
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const themeHook = useState("light");
+  return(
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Toggler />
+        <HeroSection />
+      </div>
+    </ThemeContext.Provider>
+  )
 }
 
 export default App;
